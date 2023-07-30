6f90767d27c95347cf3e472f917227c5c32b26ae
Romikumar02 <romikumar0210@gmail.com>
2023-07-30 16:12:03 +0530

    starting server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3333
+const serverconfig = require('./configs/server.config')
 app.listen(port, ()=>{
     console.log('server started');
 })
